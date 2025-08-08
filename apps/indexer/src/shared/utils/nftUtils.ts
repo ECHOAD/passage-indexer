3daa21d15c738cc9ca28a01c92ec33dc5593316c
Adrian Estevez <adrianeea.ae@gmail.com>
2025-08-08 09:28:44 -0400

    Fix mintNft function to correctly retrieve tokenId, owner, and mintPrice using msg.index

--- apps/indexer/src/shared/utils/nftUtils.ts
+++ apps/indexer/src/shared/utils/nftUtils.ts
@@ -24,7 +24,7 @@ type TxEventType =
   | "wasm-payout-royalty"
   | "wasm-payout-seller"
   | "wasm-refund-bidder";
-export function getEventAttributeValue(events: TransactionEventWithAttributes[], eventType: TxEventType, attributeKey: string, index?: number) {
-  const event = events.find((event) => event.type === eventType && (!index || event.index === index));
+export function getEventAttributeValue(events: TransactionEventWithAttributes[], eventType: TxEventType, attributeKey: string, index= 0) {
+  const event = events.find((event) => event.type === eventType && event.index === index);
   return event?.attributes.find((attr) => attr.key === attributeKey)?.value ?? null;
 }
